d3b07659af5c85a6f405a9aebd45fcb4ee32c5ca
khadkaabishek <abishekkhadka36@gmail.com>
2026-07-18 08:32:38 +0545

    fixing origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,12 +20,13 @@ const app = express();
 
 // ---- Security & core middleware ----
 app.use(helmet());
-app.use(
-  cors({
-    origin: env.cors.origin,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: env.cors.origin,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(compression());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
