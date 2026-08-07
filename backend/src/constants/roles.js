85e250122d63203fb48e2bf17e4856e3c17d420d
MadanBelbase <madanbelbase927@gmail.com>
2026-08-07 11:46:41 +0545

    Change in whole backend structure

--- backend/src/constants/roles.js
+++
@@ -1,9 +0,0 @@
-export const ROLES = Object.freeze({
-  SUPER_ADMIN: 'Super Admin',
-  TRAFFIC_POLICE: 'Traffic Police',
-  VEHICLE_OWNER: 'User',
-});
-
-export const ROLE_LIST = Object.values(ROLES);
-
-export default ROLES;
